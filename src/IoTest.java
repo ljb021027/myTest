592590dd80afb3ab83645f4ffcd8a6d16b88e52e
ljb021027 <415888451@qq.com>
2016-10-01 18:45:24 +0800

    Update IoTest.java

--- src/IoTest.java
+++ src/IoTest.java
@@ -13,9 +13,9 @@ import java.util.Random;
 public class IoTest {
 	public static void main(String[] args) {
 		long start = System.currentTimeMillis();
-		//ί����˺�����ģ��
-		File file1 = new File("C:/Users/admin/Desktop/�½��ı��ĵ�.txt");
-		//���ģ��
+		//委办局账号密码模板
+		File file1 = new File("C:/Users/admin/Desktop/新建文本文档.txt");
+		//输出模板1
 		File file2 = new File("C:/Users/admin/Desktop/out.txt");
 		BufferedReader br1 = null;
 		BufferedReader br2 = null;
@@ -24,15 +24,15 @@ public class IoTest {
 		try {
 			br1 = new BufferedReader(new FileReader(file1));
 			br2 = new BufferedReader(new FileReader(file2));
-			//����ļ�
+			//输出文件
 			bw = new BufferedWriter(new OutputStreamWriter(
 					new FileOutputStream(
-							"C:/Users/admin/Desktop/�½��ı��ĵ� (2).txt")));
+							"C:/Users/admin/Desktop/新建文本文档 (2).txt")));
 			String s1 = null;
 			String s2 = null;
 			String[] re1 = null;
 			List<String> re2 = new ArrayList<String>();
-			// ����file2 �õ����ģ��
+			// 处理file2 得到输出模板
 			while ((s2 = br2.readLine()) != null) {
 				re2.add(s2);
 			}
@@ -40,10 +40,10 @@ public class IoTest {
 			while ((s1 = br1.readLine()) != null) {
 				re1 = s1.split("\t");
 				int randNum = ra.nextInt(899999)+100000;
-				//ÿһ��ί���
+				//每一个委办局
 				for (String s : re2) {
 					s = s.replaceAll("anonymous", re1[0].trim());
-					s = s.replaceAll("\\{ί���\\}", re1[0].trim());
+					s = s.replaceAll("\\{委办局\\}", re1[0].trim());
 					if (s.endsWith("userpassword=")) {
 						s = s + randNum;
 					}
@@ -54,7 +54,7 @@ public class IoTest {
 					bw.write(s);
 					bw.write("\n");
 				}
-				System.out.println("ί�������:"+re1[1].trim()+" �˺�:"+re1[0].trim()+" ����:"+randNum);
+				System.out.println("委办局名称:"+re1[1].trim()+" 账号:"+re1[0].trim()+" 密码:"+randNum);
 				bw.write("\n");
 				//System.out.println();
 				index++;
@@ -88,7 +88,7 @@ public class IoTest {
 
 class t2 {
 	public static void main(String[] args) {
-		String s = "spypjdglj	��ҩ���";
+		String s = "spypjdglj	市药监局";
 
 		String[] re = s.split("\t");
 		for (String string : re) {
